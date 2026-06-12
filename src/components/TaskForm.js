4d095013d60446d5d1b3352194673a633f55afb7
Daniel Zhang <traceless0714@gmail.com>
2026-06-12 15:38:48 -0500

    updated taskform, now it can hold interactions

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,11 +1,15 @@
+import { useState } from 'react';
+
 function TaskForm() {
+    const [text, setText] = useState('');
     const handleSubmit = event => {
         event.preventDefault();
-
+        if(!text.trim()) return;
+        setText('');
     }
     return(
         <form className='task-form' onSubmit={handleSubmit}>
-            <input type='text' placeholder='enter task' />
+            <input type='text' placeholder='enter task' value={text} onChange={event => setText(event.target.value)}/>
             <button type='submit'>Add</button>
         </form>
     )
